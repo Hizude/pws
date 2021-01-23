abea785c83df4bb8291f09d52ec8286303b912a7
Serkan Ertas <k.serkanertas@gmail.com>
2021-01-23 15:43:20 +0100

    added font zoom

--- common/navbar.js
+++ common/navbar.js
@@ -1,5 +1,9 @@
+const root = document.querySelector('html');
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const zoomReset = document.querySelector('#zoom-reset');
+const zoomlarge = document.querySelector('#zoom-large');
+const zoomLarger = document.querySelector('#zoom-larger');
 
 burger.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
@@ -9,4 +13,29 @@ burger.addEventListener('click', () => {
   }
 })
 
-// TODO: add class md:list-inside to current page
+zoomReset.addEventListener('keyup', event => {
+  if (event.code === 'Space' || event.code === 'Enter') {
+    root.style.fontSize = 'medium'
+  }
+})
+zoomReset.addEventListener('click', () => {
+  root.style.fontSize = 'medium'
+})
+
+zoomlarge.addEventListener('keyup', () => {
+  if (event.code === 'Space' || event.code === 'Enter') {
+    root.style.fontSize = "large";
+  }
+})
+zoomlarge.addEventListener('click', () => {
+  root.style.fontSize = 'large';
+})
+
+zoomLarger.addEventListener('keyup', () => {
+  if (event.code === 'Space' || event.code === 'Enter') {
+    root.style.fontSize = "x-large";
+  }
+})
+zoomLarger.addEventListener('click', () => {
+  root.style.fontSize = 'x-large'
+})
